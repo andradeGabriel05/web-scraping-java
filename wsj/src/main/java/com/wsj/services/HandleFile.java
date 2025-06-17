e8ec282083525e7ad8007ca0997ad1b2761498cd
andradeGabriel05 <gabriel.andrade@sptech.school>
2025-06-17 12:33:53 -0300

    creating & writing into file && logger

---
+++ wsj/src/main/java/com/wsj/services/HandleFile.java
@@ -0,0 +1,41 @@
+package com.wsj.services;
+
+
+import com.wsj.Main;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+//https://www.w3schools.com/java/java_files_create.asp
+public class HandleFile {
+    Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public void createFile(String filename) {
+        try {
+            File myObj = new File("C:\\Windows\\Temp\\"+filename+".txt");
+            if (myObj.createNewFile()) {
+                logger.info("File created: {}", myObj.getName());
+            } else {
+                logger.warn("File already exists: {}", myObj.getName());
+            }
+        } catch (IOException e) {
+            logger.error("An error occurred. {}", e);
+//            e.printStackTrace();
+        }
+    }
+
+    public void writeFile(String filename, String content) {
+        try {
+            FileWriter myWriter = new FileWriter("C:\\Windows\\Temp\\"+filename+".txt",true);
+            myWriter.write(content + System.lineSeparator());
+            myWriter.close();
+            logger.info("Successfully wrote to the file;");
+        } catch (IOException e) {
+            logger.error("An error occurred. {}", e);
+//            e.printStackTrace();
+        }
+    }
+}
